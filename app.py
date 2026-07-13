69eea7e73ffb8b4f96b889e58af001f61c63f823
TrinityManiac <karan.sanjay2007@gmail.com>
2026-07-14 01:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -271,4 +271,4 @@ if __name__ == '__main__':
     print(f'  ║  Dashboard:   http://localhost:{PORT}/dashboard ║')
     print('  ╚══════════════════════════════════════╝')
     print('')
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run()
